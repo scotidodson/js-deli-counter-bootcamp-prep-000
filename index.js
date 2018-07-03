af0a9c53af2da078aef7c0306fe096507651f6ac
Scoti Dodson <scotidodson@gmail.com>
2018-07-03 20:55:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+var katzDeli = [];
+
+
+function takeANumber(katzDeliLine, name) {
+  var lineLength = katzDeliLine.length + 1;
+  var greeting = "Welcome, " + `${name}` + ". You are number " + lineLength  + " in line.";
+  katzDeliLine.push(name);
+  return greeting;
+}
+
+
+function nowServing(katzDeliLine) {
+  var lineLength = katzDeliLine.length;
+  var zeroGreeting = "There is nobody waiting to be served!";
+  var nextGreeting = "Currently serving " + katzDeliLine[0] + ".";
+  while(lineLength>0) {
+    katzDeliLine.shift(1);
+    return nextGreeting;
+  }
+  while(lineLength===0) {
+    return zeroGreeting;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  var lineLength = katzDeliLine.length;
+  var currentLine = [];
+  var emptyLine = "The line is currently empty.";
+  var theresALine = "The is currently:" + [katzDeliLine];
+
+  while(lineLength===0) {
+    return emptyLine;
+  }  
+  
+  for (, i>0, i--) {
+    
+  }
+  }
+}
+// returns current line; 
+// "The line is currently: 1. Name, 2. Grace"
+// "The line is currently empty."
